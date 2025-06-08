3918502e1a42f2e0c6ef2a1ece7f773a6873b712
shravani556 <38123454+shravani556@users.noreply.github.com>
2025-06-08 16:08:21 +0530

    Create main.tf

---
+++ terraform/vm-dev/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+#resource "aws_instance" "example" {
+#  ami           = "ami-0c94855ba95c71c99" # us-east-1
+#  instance_type = "t2.micro"
+
+#  tags = {
+#    Name = "Dev-Instance-shr"
+#  }
+#}
