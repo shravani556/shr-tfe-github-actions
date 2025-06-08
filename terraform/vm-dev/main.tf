3edd30159a8125da91cc26205adc0fe450955dfc
shravani556 <38123454+shravani556@users.noreply.github.com>
2025-06-08 22:47:00 +0530

    Update main.tf

--- terraform/vm-dev/main.tf
+++ terraform/vm-dev/main.tf
@@ -2,11 +2,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0c94855ba95c71c99" # us-east-1
-  instance_type = "t2.micro"
+# resource "aws_instance" "example" {
+  #ami           = "ami-0c94855ba95c71c99" # us-east-1
+  #instance_type = "t2.micro"
 
-  tags = {
-    Name = "Dev-Instance-shr"
-  }
-}
+  #tags = {
+   # Name = "Dev-Instance-shr"
+  #}
+#}
